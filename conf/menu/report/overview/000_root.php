dfcc160272de5893caf228b4ef7d6ff114a83082
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-07-21 13:31:02 +0200

    imported menus

---
+++ conf/menu/report/overview/000_root.php
@@ -0,0 +1,11 @@
+<?php
+
+$this->firstEntry = array
+(
+  'menu_webfrap_root',
+  Wgt::MAIN_TAB,
+  '..',
+  'webfrap root',
+  'maintab.php?c=Webfrap.Navigation.explorer',
+  WgtIcon::big('places/folder.png'),
+);
