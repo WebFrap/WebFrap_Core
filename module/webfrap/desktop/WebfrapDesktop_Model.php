26f6602f837fb8bfc1b06778b627cf2e1a4db2ea
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-09-18 23:43:33 +0200

    added menu loader

--- module/webfrap/desktop/WebfrapDesktop_Model.php
+++ module/webfrap/desktop/WebfrapDesktop_Model.php
@@ -24,5 +24,68 @@
 class WebfrapDesktop_Model extends Model
 {
 
+  /**
+   * @param int $profileId
+   * @return array[rowid, label, icon, http_url]
+   */
+  public function getProfileMenu( $profileId )
+  {
+
+    $db = $this->getDb();
+
+    $sql = <<<SQL
+
+SELECT
+  rowid,
+  label,
+  icon,
+  http_url
+FROM
+  wbfsys_menu_entry
+JOIN
+  wbfsys_profile
+    ON wbfsys_profile.id_profile_menu = wbfsys_menu_entry.id_menu
+WHERE
+  wbfsys_profile.rowid = {$profileId};
+
+SQL;
+
+    $db->select($sql)->getAll();
+
+
+  }//end public function getProfileMenu */
+
+  /**
+   * @param int $profileId
+   * @return array[rowid, label, icon, http_url]
+   */
+  public function getMainMenu( $profileId )
+  {
+
+    $db = $this->getDb();
+
+    $sql = <<<SQL
+
+SELECT
+  rowid,
+  label,
+  icon,
+  http_url,
+  m_parent
+FROM
+  wbfsys_menu_entry
+JOIN
+  wbfsys_profile
+    ON wbfsys_profile.id_main_menu = wbfsys_menu_entry.id_menu
+WHERE
+  wbfsys_profile.rowid = {$profileId};
+
+SQL;
+
+    $db->select($sql)->getAll();
+
+
+  }//end public function getProfileMenu */
+
 } // end class WebfrapDesktop_Model
 
